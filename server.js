c85e38ce4e9889ab4293a601a60b108c432f02aa
Akshay Shelke <akshay.s.shelke15@gmail.com>
2018-02-06 11:00:22 +0530

    Society Admin 6-2-2018

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ var app =express();
 
 const superadminroute = require('./routes/superadminroute');
 
-
 //connect to mongodb
-mongoose.connect('mongodb://akshay:akshay@ds119028.mlab.com:19028/visitant');
+mongoose.connect('mongodb://cybercode:cybercode@ds119028.mlab.com:19028/visitant');
 
 //on connection 
 mongoose.connection.on('connected', ()=>{
