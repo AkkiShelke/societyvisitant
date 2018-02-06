049e9db0f75cea81b9dfe72a01234109772bcbe3
Akshay Shelke <akshay.s.shelke15@gmail.com>
2018-02-06 16:27:02 +0530

    Society Admin 6-2-2018

--- public/flatowner-details.module.chunk.js
+++ public/flatowner-details.module.chunk.js
@@ -1,5 +1,44 @@
 webpackJsonp(["flatowner-details.module"],{
 
+/***/ "../../../../../src/app/layout/flatowner-details/flatowner-details-routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FlatownerDetailsRoutingModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__flatowner_details_component__ = __webpack_require__("../../../../../src/app/layout/flatowner-details/flatowner-details.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_router__ = __webpack_require__("../../../router/esm5/router.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    {
+        path: '',
+        component: __WEBPACK_IMPORTED_MODULE_1__flatowner_details_component__["a" /* FlatownerDetailsComponent */]
+    }
+];
+var FlatownerDetailsRoutingModule = (function () {
+    function FlatownerDetailsRoutingModule() {
+    }
+    FlatownerDetailsRoutingModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+            imports: [__WEBPACK_IMPORTED_MODULE_2__angular_router__["c" /* RouterModule */].forChild(routes)],
+            exports: [__WEBPACK_IMPORTED_MODULE_2__angular_router__["c" /* RouterModule */]]
+        })
+    ], FlatownerDetailsRoutingModule);
+    return FlatownerDetailsRoutingModule;
+}());
+
+
+
+/***/ }),
+
 /***/ "../../../../../src/app/layout/flatowner-details/flatowner-details.component.html":
 /***/ (function(module, exports) {
 
@@ -70,6 +109,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/esm5/common.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__flatowner_details_component__ = __webpack_require__("../../../../../src/app/layout/flatowner-details/flatowner-details.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__flatowner_details_routing_module__ = __webpack_require__("../../../../../src/app/layout/flatowner-details/flatowner-details-routing.module.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -79,13 +119,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
 var FlatownerDetailsModule = (function () {
     function FlatownerDetailsModule() {
     }
     FlatownerDetailsModule = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
             imports: [
-                __WEBPACK_IMPORTED_MODULE_1__angular_common__["CommonModule"]
+                __WEBPACK_IMPORTED_MODULE_1__angular_common__["CommonModule"],
+                __WEBPACK_IMPORTED_MODULE_3__flatowner_details_routing_module__["a" /* FlatownerDetailsRoutingModule */]
             ],
             declarations: [__WEBPACK_IMPORTED_MODULE_2__flatowner_details_component__["a" /* FlatownerDetailsComponent */]]
         })
