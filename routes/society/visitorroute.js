5eda11a21e8a72800ef2e741399841c29e4d3fce
Akshay Shelke <akshay.s.shelke15@gmail.com>
2018-02-15 13:32:58 +0530

    Society Admin 15-2-2018

--- routes/society/visitorroute.js
+++ routes/society/visitorroute.js
@@ -55,6 +55,8 @@ var storage = multer.diskStorage({
     // destination: function (req, file, callback) {
     //     callback(null, './uploads')
     // },
+
+    
     filename: function (req, file, callback) {
         callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
